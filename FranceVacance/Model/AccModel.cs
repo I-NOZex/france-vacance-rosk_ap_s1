171a63e164a5c2705c71e4d7fa93ca9f58d923aa
bori0409 <44772249+bori0409@users.noreply.github.com>
2018-11-07 17:37:20 +0200

    model
    I tried to do a model about the acc

---
+++ FranceVacance/Model/AccModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FranceVacance.Model
+{
+    class AccModel
+
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public bool availability { get; set; }
+        public double price { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Rating { get; set; }
+
+
+        public AccModel()
+        {
+            
+        }
+
+    }
+}

4f037a3338a20bad3282bef5392630f419600e20
Tiago Marques <simplesmente_moi@hotmail.com>
2018-11-08 11:01:41 +0100

    BOOKING

--- FranceVacance/MainPage.xaml.cs
+++ FranceVacance/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FranceVacance.Data;
+using FranceVacance.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,8 +24,8 @@ namespace FranceVacance
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
-        {
+        public MainPage() {
+            var a = new AccommodationCatalog();
             this.InitializeComponent();
         }
     }

3db970fbf20d252d2893c254a8a65138fc5cdf06
Tiago Marques <simplesmente_moi@hotmail.com>
2018-11-22 15:53:10 +0100

    logic to enforce the checkout date must be higher than checkin

---
+++ FranceVacance/Helpers/DateOperationsConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace FranceVacance.Helpers {
+    public class DateOperationsConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            string operation = parameter as string;
+            DateTimeOffset date = value is DateTimeOffset ? (DateTimeOffset) value : new DateTimeOffset();
+
+            if(operation == "AddOneDay")
+                return date.AddDays(1);
+            else if (operation == "SubtractOneDay")
+                return date.Subtract(TimeSpan.FromDays(1));
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}

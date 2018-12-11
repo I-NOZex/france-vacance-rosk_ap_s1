3b73d2de160c1a146e99675f7cb96d1884aace03
vanio15 <ivanmenikarov@gmail.com>
2018-12-11 16:35:43 +0100

    new class VmCreatingAccommodation

---
+++ FranceVacance/Code/Accommodation/VmCreatingAccommodation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FranceVacance.Code.Common;
+using FranceVacance.Code.Helpers;
+using FranceVacance.Code.User;
+
+namespace FranceVacance.Code.Accommodation
+{
+    class VmCreatingAccommodation : ViewModelBase
+    {
+        private string _vname;
+        private double _vprice;
+        private string _vaddress;
+        private AccommodationService _accommodationService;
+        private AccommodationModel _accommodationModel;
+        private RelayCommand Creatingaccommodation { get; set; }
+
+        public string Vname
+        {
+            get { return _vname; }
+            set
+            {
+                _vname = value;
+                OnPropertyChanged("Vname");
+            }
+        }
+
+        public double Vprice
+        {
+            get { return _vprice; }
+            set
+            {
+                _vprice = value;
+                OnPropertyChanged("Vprice");
+            }
+        }
+
+        public string Vaddress
+        {
+            get { return _vaddress; }
+            set
+            {
+                _vaddress = value;
+                OnPropertyChanged("Vaddress");
+            }
+        }
+
+        public VmCreatingAccommodation()
+        {
+           Creatingaccommodation =new RelayCommand(AddAccommodation);
+            Vprice = Convert.ToDouble("");
+            Vaddress = "";
+            Vname = "";
+
+
+        }
+
+        private void AddAccommodation()
+        {
+            var accomodation=AccommodationViewModel.FirstInstance;
+            if (ValidateData())
+            {
+                AccommodationModel newAccommodation=new AccommodationModel();
+                newAccommodation.Price = Vprice;
+                newAccommodation.Address = Vaddress;
+                newAccommodation.Name = Vname;
+        
+
+            }
+        }
+    }
+}
--- Tests/UserViewModelTest.cs
+++ Tests/UserViewModelTest.cs
@@ -1,4 +1,7 @@
-﻿using FranceVacance.Code.User;
+﻿using System;
+using System.IO;
+using System.ServiceModel.Security;
+using FranceVacance.Code.User;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -7,7 +10,7 @@ namespace Tests
     public class UserViewModelTest
     {
         [TestMethod]
-        public void Login()
+        public void Login_Success()
         {
 
             UserViewModel.Instance.RegisteredUsers.Add(new UserModel()
@@ -20,12 +23,19 @@ namespace Tests
             LoginViewModel LoginVM = new LoginViewModel();
             LoginVM.Username = $"username";
             LoginVM.Password = $"password";
+           
+            
+            //RegisterUserViewModel RegistrVM=new RegisterUserViewModel();
+            //RegistrVM.UserError = "";
+           
 
-            LoginVM.Login();
+            var result = LoginVM.Login();
 
             Assert.IsNotNull(UserViewModel.Instance.CurrentUser);
             Assert.AreEqual(UserViewModel.Instance.CurrentUser.Username, $"username");
             Assert.AreEqual(UserViewModel.Instance.CurrentUser.Password, $"password");
+            Assert.AreEqual(UserViewModel.Instance.CurrentUser.Username,$"username not in the system");
+            Assert.IsFalse(result);
         }
     }
 }
